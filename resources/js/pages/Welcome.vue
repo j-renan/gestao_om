4f0406892fe42e35b3be171203138fa490780ab9
j-renan <desen.renan@gmail.com>
2025-09-05 10:35:15 -0300

    Personalizando tela inicial

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -10,9 +10,9 @@ import { Head, Link } from '@inertiajs/vue3';
     </Head>
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100  text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
-        <div class="navbar  mb-6 shadow-sm">
+        <div class="navbar border bg-white border-red-500 mb-6 shadow-sm">
             <div class="flex-1">
-                <a class="btn btn-ghost text-xl">SENAI-OM</a>
+                <a class="card-title text-xl text-red-600">SENAI-OM</a>
             </div>
             <div class="flex-none">
                 <nav class="flex items-center justify-end gap-4">
@@ -22,26 +22,27 @@ import { Head, Link } from '@inertiajs/vue3';
                     </Link>
                     <template v-else>
                         <Link :href="route('login')"
-                            class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]">
+                            class="inline-block rounded-sm border shadow-sm border-red-500 px-5 py-1.5 text-sm leading-normal transition delay-100 ease-in-out text-red-600 hover:scale-110 hover:border-red-800 hover:bg-red-400 hover:text-white hover:shadow-md dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]">
                         Entrar
                         </Link>
-                        <Link :href="route('register')"
+                        <!-- <Link :href="route('register')"
                             class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
                         Cadastre-se
-                        </Link>
+                        </Link> -->
                     </template>
                 </nav>
             </div>
         </div>
 
         <div class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-            <div class="card bg-base-100 w-96 shadow-sm">
+            <div class="card bg-base-100 w-96 h-96
+             shadow-md">
                 <div class="card-body">
-                    <h2 class="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <h2 class="card-title">SENAI-OM</h2>
+                    <p>Bem vindo ao sistema SENAI para gerenciamento de ordens de manutenção</p>
                 </div>
                 <figure>
-                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                    <img src="https://yt3.googleusercontent.com/wyGnsuVLCBoHStdhQ3Tj7Wr48yb_Oi2e1OmP2Rly99xB6wwe66T64bhCNDZkP5xxNHxF-lsE1A=s900-c-k-c0x00ffffff-no-rj" alt="Shoes" />
                 </figure>
             </div>
         </div>
