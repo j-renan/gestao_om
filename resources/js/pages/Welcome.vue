10398dbd063e8e8bead4afc7ba5c31bfd2518623
j-renan <desen.renan@gmail.com>
2025-09-05 08:25:59 -0300

    Subindo projeto inicial

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div
+        class="flex min-h-screen flex-col items-center bg-gray-100  text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
+        <div class="navbar  mb-6 shadow-sm">
+            <div class="flex-1">
+                <a class="btn btn-ghost text-xl">SENAI-OM</a>
+            </div>
+            <div class="flex-none">
+                <nav class="flex items-center justify-end gap-4">
+                    <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
+                    Dashboard
+                    </Link>
+                    <template v-else>
+                        <Link :href="route('login')"
+                            class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]">
+                        Entrar
+                        </Link>
+                        <Link :href="route('register')"
+                            class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
+                        Cadastre-se
+                        </Link>
+                    </template>
+                </nav>
+            </div>
+        </div>
+
+        <div class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
+            <div class="card bg-base-100 w-96 shadow-sm">
+                <div class="card-body">
+                    <h2 class="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                </div>
+                <figure>
+                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                </figure>
+            </div>
+        </div>
+    </div>
+</template>
