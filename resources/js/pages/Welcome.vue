9ba5b88eb73ada136306e9cf0aa9fd53d22637f2
j-renan <desen.renan@gmail.com>
2025-09-05 14:35:41 -0300

    Personalizando tela de login

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -11,8 +11,11 @@ import { Head, Link } from '@inertiajs/vue3';
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100  text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
         <div class="navbar border bg-white border-red-500 shadow-sm">
+            <div class="flex w-30 items-center justify-center rounded-md">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUU9uH4-kpTYm4GxBN9IzsVD4WRLpYhpFbPQ&s" alt="senai">
+            </div>
             <div class="flex-1">
-                <a class="card-title text-xl text-red-600">SENAI-OM</a>
+                <a class="card-title text-xl text-red-600"> - OM</a>
             </div>
             <div class="flex-none">
                 <nav class="flex items-center justify-end gap-4">
@@ -25,10 +28,10 @@ import { Head, Link } from '@inertiajs/vue3';
                             class="inline-block rounded-sm border shadow-sm border-red-500 px-5 py-1.5 text-sm leading-normal transition delay-100 ease-in-out text-red-600 hover:scale-110 hover:border-red-800 hover:bg-red-400 hover:text-white hover:shadow-md dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]">
                         Entrar
                         </Link>
-                        <!-- <Link :href="route('register')"
+                        <Link :href="route('register')"
                             class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
                         Cadastre-se
-                        </Link> -->
+                        </Link>
                     </template>
                 </nav>
             </div>
@@ -36,8 +39,7 @@ import { Head, Link } from '@inertiajs/vue3';
 
         <div
             class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-            <div class="card bg-base-100 w-96 h-96
-             shadow-md">
+            <div class="card bg-base-100 w-96 h-96 shadow-md">
                 <div class="card-body">
                     <h2 class="card-title">SENAI-OM</h2>
                     <p>Bem vindo ao sistema SENAI para gerenciamento de ordens de manutenção</p>
@@ -48,7 +50,8 @@ import { Head, Link } from '@inertiajs/vue3';
                 </figure>
             </div>
         </div>
-        <footer class="footer border border-gray-300 sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+        <footer
+            class="footer border border-gray-300 sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
             <aside>
                 <p>Desenvolvido por João Renan Celso. SENAI 5.03</p>
             </aside>
--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -28,66 +28,57 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthBase title="Log in to your account" description="Enter your email and password below to log in">
-        <Head title="Log in" />
+    <div class="min-h-screen flex items-center justify-center p-4 bg-gray-100 dark:bg-gray-900">
+        <div class="rounded-lg text-card-foreground w-full max-w-md bg-white shadow-2xl border border-red-500">
+            <AuthBase title="Entre em sua conta" description="Digite seu e-mail e senha para acessar sua conta">
 
-        <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
-            {{ status }}
-        </div>
+                <Head title="Log in" />
 
-        <form @submit.prevent="submit" class="flex flex-col gap-6">
-            <div class="grid gap-6">
-                <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        required
-                        autofocus
-                        :tabindex="1"
-                        autocomplete="email"
-                        v-model="form.email"
-                        placeholder="email@example.com"
-                    />
-                    <InputError :message="form.errors.email" />
+                <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
+                    {{ status }}
                 </div>
 
-                <div class="grid gap-2">
-                    <div class="flex items-center justify-between">
-                        <Label for="password">Password</Label>
-                        <TextLink v-if="canResetPassword" :href="route('password.request')" class="text-sm" :tabindex="5">
-                            Forgot password?
-                        </TextLink>
+                <form @submit.prevent="submit" class="flex flex-col gap-6">
+                    <div class="grid gap-6">
+                        <div class="grid gap-2">
+                            <Label for="email">Email</Label>
+                            <Input id="email" type="email" required autofocus :tabindex="1" autocomplete="email" 
+                                v-model="form.email" placeholder="email@examplo.com" />
+                            <InputError :message="form.errors.email" />
+                        </div>
+
+                        <div class="grid gap-2">
+                            <div class="flex items-center justify-between">
+                                <Label for="password">Senha</Label>
+                                <TextLink v-if="canResetPassword" :href="route('password.request')" class="text-sm"
+                                    :tabindex="5">
+                                    Esqueceu sua senha?
+                                </TextLink>
+                            </div>
+                            <Input id="password" type="password" required :tabindex="2" autocomplete="current-password"
+                                v-model="form.password" placeholder="Senha" />
+                            <InputError :message="form.errors.password" />
+                        </div>
+
+                        <div class="flex items-center justify-between">
+                            <Label for="remember" class="flex items-center space-x-3">
+                                <Checkbox id="remember" v-model="form.remember" :tabindex="3" />
+                                <span>Lembrar conta</span>
+                            </Label>
+                        </div>
+
+                        <Button type="submit" class="mt-4 w-full bg-red-400 border shadow-sm border-red-800 px-5 py-1.5 text-sm leading-normal transition delay-100 ease-in-out text-white hover:scale-105 hover:border-red-900 hover:bg-red-600 hover:text-white hover:shadow-md" :tabindex="4" :disabled="form.processing">
+                            <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
+                            Log in
+                        </Button>
                     </div>
-                    <Input
-                        id="password"
-                        type="password"
-                        required
-                        :tabindex="2"
-                        autocomplete="current-password"
-                        v-model="form.password"
-                        placeholder="Password"
-                    />
-                    <InputError :message="form.errors.password" />
-                </div>
 
-                <div class="flex items-center justify-between">
-                    <Label for="remember" class="flex items-center space-x-3">
-                        <Checkbox id="remember" v-model="form.remember" :tabindex="3" />
-                        <span>Remember me</span>
-                    </Label>
-                </div>
-
-                <Button type="submit" class="mt-4 w-full" :tabindex="4" :disabled="form.processing">
-                    <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                    Log in
-                </Button>
-            </div>
-
-            <div class="text-center text-sm text-muted-foreground">
-                Don't have an account?
-                <TextLink :href="route('register')" :tabindex="5">Sign up</TextLink>
-            </div>
-        </form>
-    </AuthBase>
+                    <!-- <div class="text-center text-sm text-muted-foreground">
+                        Don't have an account?
+                        <TextLink :href="route('register')" :tabindex="5">Sign up</TextLink>
+                    </div> -->
+                </form>
+            </AuthBase>
+        </div>
+    </div>
 </template>
